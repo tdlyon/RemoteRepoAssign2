f031d9190fc1d6dc3dfc5bffa6f896e1b18475a7
Trevor Lyon <tdlyon@oakland.edu>
2016-02-04 16:10:22 -0500

    Added beer song

---
+++ BeerSong.java
@@ -0,0 +1,18 @@
+public class BeerSong {
+
+   
+    public static void main(String[] args) {
+        Nintey_Nine_Bottles_of_Beer();
+        System.out.println("1 more bottle of beer on the wall, one more bottle of beer!");
+        System.out.println("Take one down, pass it around, no more bottles of beer on the wall!");
+    }
+    public static void Nintey_Nine_Bottles_of_Beer() {
+        int i;
+        for (i=99; i > 1;) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
+            i = i-1;
+            System.out.println("Take one down, pass it around, " + i + " bottles of beer on the wall!");
+        }
+    }
+    
+}
